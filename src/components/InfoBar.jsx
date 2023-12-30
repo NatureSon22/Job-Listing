5d2df20cddea3fc9c594c2fcaf5eee711806d22f
Nature Son <110081044+NatureSon22@users.noreply.github.com>
2023-12-30 07:35:46 +0700

    Add files via upload

--- src/components/InfoBar.jsx
+++ src/components/InfoBar.jsx
@@ -1,12 +1,15 @@
 import CompanyDetail from "./CompanyDetail"
 import FilterList from "./FilterList"
+import { IMAGES } from "../data-img"
 
-const InfoBar = ({ data: {company, logo, new: isNew, featured: isFeatured, position, role, level, postedAt, contract,
+
+const InfoBar = ({ index, data: {company, logo, new: isNew, featured: isFeatured, position, role, level, postedAt, contract,
     location, languages, tools}, setFilter}) => {
 
+    const logoImg = IMAGES[index].logo;  
     const details = { 
         company, 
-        logo,
+        logo: logoImg,
         isNew,
         isFeatured, 
         position,
@@ -16,7 +19,6 @@ const InfoBar = ({ data: {company, logo, new: isNew, featured: isFeatured, posit
     }
 
     const requirements = [ role, level, ...languages, ...tools ];
-
 
     return (
         <div className={`info-bar ${isFeatured ? 'featured' : null }`}>
