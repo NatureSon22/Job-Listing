50bdada461e5c876501a4729233b0b74e3e506ed
Nature Son <110081044+NatureSon22@users.noreply.github.com>
2023-12-30 07:33:18 +0700

    Add files via upload

---
+++ src/data-img.js
@@ -0,0 +1,43 @@
+import photosnap from '../images/photosnap.svg'
+import manage from '../images/manage.svg'
+import account from '../images/account.svg'
+import myhome from '../images/myhome.svg'
+import loopstudios from '../images/loopstudios.svg'
+import faceit from '../images/faceit.svg'
+import shortly from '../images/shortly.svg'
+import insure from '../images/insure.svg'
+import eyecamco from '../images/eyecamco.svg'
+import airfilter from '../images/airfilter.svg'
+
+export const IMAGES = [
+    {
+        logo: photosnap
+    },
+    {
+        logo: manage
+    },
+    {
+        logo: account
+    },
+    {
+        logo: myhome
+    },
+    {
+        logo: loopstudios
+    },
+    {
+        logo: faceit
+    },
+    {
+        logo: shortly
+    },
+    {
+        logo: insure
+    },
+    {
+        logo: eyecamco
+    },
+    {
+        logo: airfilter
+    },
+];
